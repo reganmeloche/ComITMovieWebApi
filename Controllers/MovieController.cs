f74f8be12c321f004b81dc3e3caea753d8568cd8
Regan Meloche <reganmeloche@gmail.com>
2020-07-17 12:43:15 -0600

    first commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MovieWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        List<Movie> _movieList;
+
+        public MovieController(List<Movie> movieList)
+        {
+            _movieList = movieList;
+        }
+
+        [HttpGet]
+        public List<Movie> GetAllMovies()
+        {
+            return _movieList;
+        }
+
+        [HttpPost]
+        public string CreateMovie(Movie myNewMovie)
+        {   
+            _movieList.Add(myNewMovie);
+            return "Creating movie";
+        }
+
+        // Next week...
+        [HttpPut]
+        public string UpdateMovie()
+        {
+            return "Updating movie";
+        }
+
+        [HttpDelete]
+        public string DeleteMovie()
+        {
+            return "Deleting movie";
+        }
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MovieWebApi
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        public int Year { get; set; }
+    }
+}
